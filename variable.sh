66499967237c1688adc1317fbd5ba5b6aa6cfb52
jeevan <jeev5324u@gmail.con>
2024-02-16 14:16:55 +0530

    MJK-105-VARIABLE

---
+++ variable.sh
@@ -0,0 +1,14 @@
+## variable is if we assigh=n a anme to set of data is called as variable
+
+
+
+trainer=raghu
+class=devops
+
+
+## to access a variable done in two ways ---> $name or ${name}
+
+
+echo "trainer name is $trainer"
+echo class name is ${class}
+

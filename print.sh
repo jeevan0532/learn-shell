4060360a6a555ce3c9a51d45461b524c7b8cf786
jeevan <jeev5324u@gmail.con>
2024-02-16 14:07:52 +0530

    Mjk-104-colour code

--- print.sh
+++ print.sh
@@ -1,3 +1,7 @@
 ehco hello world
 echo "hii this is jeevan and i am here to explain somwthing to you"
-echo ****** LOVE YOU NIKHI ******
+echo "****** LOVE YOU NIKHI ******"
+
+#color code syntax is --->  echo -e "\e[COLm<message>\e[0m"
+
+echo -e "\e[35miloveinda\e[0m"
